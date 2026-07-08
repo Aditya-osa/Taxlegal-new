00215f517a79a252ce20b16a74c65b9889fb835f
Prem386 <premkadam0506@gmail.com>
2026-07-08 15:37:36 +0530

    removed start rating form the testimonials

--- src/components/common/TestimonialModal.jsx
+++ src/components/common/TestimonialModal.jsx
@@ -78,11 +78,6 @@ const TestimonialModal = ({ testimonial, onClose }) => {
         tabIndex="-1"
       >
         <div className="testimonial-modal-header">
-          <div className="testimonial-modal-stars" aria-label={`Rating: ${testimonial.rating || 5} out of 5 stars`}>
-            {[...Array(testimonial.rating || 5)].map((_, i) => (
-              <svg key={i} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#FFC107" stroke="#FFC107" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-            ))}
-          </div>
           <button 
             type="button" 
             className="testimonial-modal-close" 
